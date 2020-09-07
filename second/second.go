ac9f6d2fa1d29b05c4e70f99e76b75234ebb88c7
peterhero <63244012+peterhero@users.noreply.github.com>
2020-09-07 22:25:38 +0200

    Second session of learning Go
    Methods, Pointer Receivers, More web dev basics. Next is going to be Accessing the internet - https://www.youtube.com/playlist?list=PLQVvvaa0QuDeF3hP0wQoSxpkqgRcgxMqX

--- second/second.go
+++ second/second.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+const u16max = 65535
+const kmhMultiple float64 = 1.60934
+
 type car struct {
 	gasPedal      uint16
 	brakePedal    uint16
@@ -9,11 +12,29 @@ type car struct {
 	topSpeedKmh   float64
 }
 
+func (c car) kmh() float64{
+	return float64(c.gasPedal) * (c.topSpeedKmh/float64(u16max))
+}
+
+func (c car) mph() float64{
+	return float64(c.gasPedal) * (c.topSpeedKmh/float64(u16max))/kmhMultiple
+}
+
+func (c *car) newTopSpeed(newSpeed float64) {
+	c.topSpeedKmh = newSpeed
+}
+
 func main() {
-	car1 := car{gasPedal: 22341, // possible values only
+	car1 := car{gasPedal: 65000, // possible values only
 		brakePedal:    0, // but this is better way
 		steeringWheel: 12561,
 		topSpeedKmh:   225.0}
 
 	fmt.Println(car1.gasPedal)
+	fmt.Println(car1.kmh())
+	fmt.Println(car1.mph())
+
+	car1.newTopSpeed(500)
+	fmt.Println(car1.kmh())
+	fmt.Println(car1.mph())
 }
---
+++ webapp2/webapp2.go
@@ -0,0 +1,22 @@
+package main
+
+import ("fmt"
+		"net/http")
+
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	
+	fmt.Fprintf(w, `<h1>Hey there</h1>
+<p>Go is fast!</p>
+<p>...and simple!</p>`)
+
+	fmt.Fprintf(w, "<h1>Hey there</h1>")
+	fmt.Fprintf(w, "<p>Go is fast!</p>")
+	fmt.Fprintf(w, "<p>...and simple!</p>")
+	fmt.Fprintf(w, "<p>You %s even add %s</p>", "can", "<strong>variables</strong>")
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":8000", nil)
+}
